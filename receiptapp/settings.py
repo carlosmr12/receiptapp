83606f1dd74e3a0f737f90d284bbb83e9a84e9bf
Carlos Rodrigues <carlosmr12@gmail.com>
2026-06-06 17:53:55 +1000

    Refactor settings to fix collectstatic path

--- receiptapp/settings.py
+++ receiptapp/settings.py
@@ -136,15 +136,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+GS_BUCKET_NAME = os.environ.get('GS_BUCKET_NAME')
 
-if not DEBUG:
+if GS_BUCKET_NAME:
+    # Production settings: Use Google Cloud Storage
     STATICFILES_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
-    GS_BUCKET_NAME = os.environ.get('GS_BUCKET_NAME')
     GS_DEFAULT_ACL = 'publicRead'
-    GS_LOCATION = 'australia-southeast2'
     STATIC_URL = f'https://storage.googleapis.com/{GS_BUCKET_NAME}/static/'
+else:
+    # Development settings: Use local static files
+    STATIC_URL = 'static/'
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
