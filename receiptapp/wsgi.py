06f490eadf78b0ba9df99351bc7ccee7cd1ac139
Carlos Rodrigues <carlosmr12@gmail.com>
2026-06-06 16:57:38 +1000

    Refactor wsgi and GCP deploy to fix startup crash in production

--- receiptapp/wsgi.py
+++ receiptapp/wsgi.py
@@ -3,14 +3,9 @@ WSGI project for receiptapp.
 """
 
 import os
+
 from django.core.wsgi import get_wsgi_application
-from django.core.management import execute_from_command_line
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'receiptapp.settings')
 
-if os.environ.get('IS_MIGRATING') == 'true':
-    execute_from_command_line(['manage.py', 'migrate'])
-elif os.environ.get('IS_COLLECTING_STATIC') == 'true':
-    execute_from_command_line(['manage.py', 'collectstatic', '--noinput'])
-else:
-    application = get_wsgi_application()
+application = get_wsgi_application()
