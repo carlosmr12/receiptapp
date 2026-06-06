f82c0c02e98ca71fe4203df9315fb7f35f609637
Carlos Rodrigues <carlosmr12@gmail.com>
2026-06-06 18:38:25 +1000

    Refactor settings_prod to fix collectstatic in production

--- receiptapp/settings_prod.py
+++ receiptapp/settings_prod.py
@@ -1,25 +1,19 @@
 # Import all settings from the base settings file
 from .settings import *
+import os
 
-# --- PRODUCTION STATIC FILES OVERRIDE ---
+# --- UNCONDITIONAL PRODUCTION STATIC FILES OVERRIDE ---
 # This configuration is used by the collectstatic command in the CI/CD pipeline.
+
+# Tell django-storages to use the GCS backend.
+STATICFILES_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 
 # Get the bucket name from the environment variable.
 # This is fetched from GCP Secret Manager in the deploy.yml workflow.
-GCS_BUCKET_NAME = os.environ.get('GS_BUCKET_NAME')
+GS_BUCKET_NAME = os.environ.get('GS_BUCKET_NAME')
 
-if GCS_BUCKET_NAME:
-    # Tell django-storages to use the GCS backend
-    STATICFILES_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+# Set the default ACL to public-read so files are accessible.
+GS_DEFAULT_ACL = 'publicRead'
 
-    # Set the name of the bucket for django-storages
-    GS_BUCKET_NAME = GCS_BUCKET_NAME
-
-    # Set the default ACL to public-read so files are accessible
-    GS_DEFAULT_ACL = 'publicRead'
-
-    # Set the URL to serve static files from
-    STATIC_URL = f'https://storage.googleapis.com/{GCS_BUCKET_NAME}/static/'
-else:
-    # If the bucket name isn't set, fail loudly during deployment.
-    raise Exception("GS_BUCKET_NAME environment variable not set for production settings.")
+# Set the URL to serve static files from.
+STATIC_URL = f'https://storage.googleapis.com/{GS_BUCKET_NAME}/static/'
