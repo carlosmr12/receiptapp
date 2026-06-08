3c582f9ce0d0addf4c9cea81fb0ec0db034e0a03
Carlos Rodrigues <carlosmr12@gmail.com>
2026-06-08 18:17:23 +1000

    Add configuration for logging in production

--- receiptapp/settings_prod.py
+++ receiptapp/settings_prod.py
@@ -121,3 +121,17 @@ USE_TZ = True
 # Session timeout settings
 SESSION_COOKIE_AGE = 2 * 60 * 60  # 2 hours
 SESSION_SAVE_EVERY_REQUEST = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO', # Capture INFO, WARNING, ERROR, CRITICAL
+    },
+}
