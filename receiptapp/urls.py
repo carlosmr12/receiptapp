bfc02add2d440df8ae637ac87ba3f7226fd4d698
Carlos Rodrigues <carlosmr12@gmail.com>
2026-06-08 12:22:40 +1000

    Add login template

--- receiptapp/urls.py
+++ receiptapp/urls.py
@@ -16,14 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView # Import RedirectView
+from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')), # Add Django auth URLs
     path('receipts/', include('receipts.urls')),
-    path('', RedirectView.as_view(pattern_name='dashboard', permanent=False)), # Redirect root to dashboard
+    path('', RedirectView.as_view(pattern_name='dashboard', permanent=False)),
 ]
 
 if settings.DEBUG:
